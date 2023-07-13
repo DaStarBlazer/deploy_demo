3b7801025eb43f8042a3dffa8ad9f81b4bc1333d
Eros Peschiera <eros.peschiera@gmail.com>
2023-07-13 12:59:15 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var ejs = require('ejs');
+var porta = 3000;
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render('home');
+})
+
+
+app.get("/about", function(req, res){
+    res.render('about');
+})
+
+app.listen(porta, function() {
+    console.log("YelpCamp is running on port: " + porta);
+})
